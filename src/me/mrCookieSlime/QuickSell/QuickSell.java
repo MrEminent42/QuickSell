5fa98260bcafd1c8821823ed5fbf08f570e8f1c5
MrEminent42 <MrEminent42@users.noreply.github.com>
2017-12-21 12:53:38 +0800

    Patch 2 (#2)
    * Replaced old config value with new shop.enable-hierarchy
    
    Previously, some methods in the onEnable() method called for the old options.open-only-shop-with-permission config value, which is now updated to shop.enable-hierarchy.
    
    * Update plugin.yml
    
    Updated version numbers and authors
    
    * Update plugin.yml

--- src/me/mrCookieSlime/QuickSell/QuickSell.java
+++ src/me/mrCookieSlime/QuickSell/QuickSell.java
@@ -252,7 +252,7 @@ public class QuickSell extends JavaPlugin {
 						}
 						else ShopMenu.openMenu((Player) sender);
 					}
-					else if (cfg.getBoolean("options.open-only-shop-with-permission")) {
+					else if (cfg.getBoolean("shop.enable-hierarchy")) {
 						if (Shop.getHighestShop((Player) sender) != null) ShopMenu.open((Player) sender, Shop.getHighestShop((Player) sender));
 						else QuickSell.local.sendTranslation(sender, "messages.no-access", false);
 					}
@@ -275,7 +275,7 @@ public class QuickSell extends JavaPlugin {
 						}
 						else local.sendTranslation(sender, "messages.unknown-shop", false);
 					}
-					else if (cfg.getBoolean("options.open-only-shop-with-permission")) {
+					else if (cfg.getBoolean("shop.enable-hierarchy")) {
 						if (Shop.getHighestShop((Player) sender) != null) {
 							Shop.getHighestShop((Player) sender).sellall((Player) sender, "", Type.SELLALL);
 						}
@@ -300,7 +300,7 @@ public class QuickSell extends JavaPlugin {
 						}
 						else local.sendTranslation(sender, "messages.unknown-shop", false);
 					}
-					else if (cfg.getBoolean("options.open-only-shop-with-permission")) {
+					else if (cfg.getBoolean("shop.enable-hierarchy")) {
 						if (Shop.getHighestShop((Player) sender) != null) {
 							Shop.getHighestShop((Player) sender).showPrices((Player) sender);
 						}
