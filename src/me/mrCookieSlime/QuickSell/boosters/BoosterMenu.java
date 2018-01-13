b6dbf1f713e25e21174e63dcc2af6a47f1bd651f
MrEminent42 <MrEminent42@users.noreply.github.com>
2018-01-13 17:07:44 +0800

    Added new booster logic support for BoosterMenu class
    New logic will only be used if the config option is turned on.

--- src/me/mrCookieSlime/QuickSell/boosters/BoosterMenu.java
+++ src/me/mrCookieSlime/QuickSell/boosters/BoosterMenu.java
@@ -15,15 +15,15 @@ import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu.MenuClickHandler;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ClickAction;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.Item.CustomItem;
-import me.mrCookieSlime.CSCoreLibPlugin.general.String.StringUtils;
+import me.mrCookieSlime.CSCoreLibPlugin.general.Math.DoubleHandler;
 import me.mrCookieSlime.QuickSell.QuickSell;
 
 public class BoosterMenu {
-
+	
 	public static void showBoosterOverview(Player p) {
 		ChestMenu menu = new ChestMenu("§3Booster Overview");
 		
-		menu.addItem(1, new CustomItem(new MaterialData(Material.GOLD_INGOT), "§bBoosters (Money)", "§7Current Multiplier: §b" + Booster.getMultiplier(p.getUniqueId(), BoosterType.MONETARY), "", "§7\u21E8 Click for Details"));
+		menu.addItem(1, new CustomItem(new MaterialData(Material.GOLD_INGOT), "§bBoosters (Money)", "§7Current Multiplier: §b" + (QuickSell.cfg.getBoolean("boosters.use-percent-logic") ? QuickSell.toPercent(Booster.getMultiplier(p.getUniqueId(), BoosterType.MONETARY)) : DoubleHandler.getFancyDouble(Booster.getMultiplier(p.getUniqueId(), BoosterType.MONETARY))), "", "§7\u21E8 Click for Details"));
 		menu.addMenuClickHandler(1, new MenuClickHandler() {
 			
 			@Override
@@ -34,7 +34,7 @@ public class BoosterMenu {
 		});
 		
 		if (QuickSell.getInstance().isMCMMOInstalled()) {
-			menu.addItem(3, new CustomItem(new MaterialData(Material.IRON_SWORD), "§bBoosters (mcMMO)", "§7Current Multiplier: §b" + Booster.getMultiplier(p.getUniqueId(), BoosterType.MCMMO), "", "§7\u21E8 Click for Details"));
+			menu.addItem(3, new CustomItem(new MaterialData(Material.IRON_SWORD), "§bBoosters (mcMMO)", "§7Current Multiplier: §b" + (QuickSell.cfg.getBoolean("boosters.use-percent-logic") ? QuickSell.toPercent(Booster.getMultiplier(p.getUniqueId(), BoosterType.MCMMO)) : DoubleHandler.getFancyDouble(Booster.getMultiplier(p.getUniqueId(), BoosterType.MCMMO))), "", "§7\u21E8 Click for Details"));
 			menu.addMenuClickHandler(3, new MenuClickHandler() {
 				
 				@Override
@@ -46,7 +46,7 @@ public class BoosterMenu {
 		}
 		
 		if (QuickSell.getInstance().isPrisonGemsInstalled()) {
-			menu.addItem(5, new CustomItem(new MaterialData(Material.EMERALD), "§bBoosters (Gems)", "§7Current Multiplier: §b" + Booster.getMultiplier(p.getUniqueId(), BoosterType.PRISONGEMS), "", "§7\u21E8 Click for Details"));
+			menu.addItem(5, new CustomItem(new MaterialData(Material.EMERALD), "§bBoosters (Gems)", "§7Current Multiplier: §b" + (QuickSell.cfg.getBoolean("boosters.use-percent-logic") ? QuickSell.toPercent(Booster.getMultiplier(p.getUniqueId(), BoosterType.PRISONGEMS)) : DoubleHandler.getFancyDouble(Booster.getMultiplier(p.getUniqueId(), BoosterType.PRISONGEMS))), "", "§7\u21E8 Click for Details"));
 			menu.addMenuClickHandler(5, new MenuClickHandler() {
 				
 				@Override
@@ -57,7 +57,7 @@ public class BoosterMenu {
 			});
 		}
 		
-		menu.addItem(7, new CustomItem(new MaterialData(Material.EXP_BOTTLE), "§bBoosters (Experience)", "§7Current Multiplier: §b" + Booster.getMultiplier(p.getUniqueId(), BoosterType.EXP), "", "§7\u21E8 Click for Details"));
+		menu.addItem(7, new CustomItem(new MaterialData(Material.EXP_BOTTLE), "§bBoosters (Experience)", "§7Current Multiplier: §b" + (QuickSell.cfg.getBoolean("boosters.use-percent-logic") ? QuickSell.toPercent(Booster.getMultiplier(p.getUniqueId(), BoosterType.EXP)) : DoubleHandler.getFancyDouble(Booster.getMultiplier(p.getUniqueId(), BoosterType.EXP))), "", "§7\u21E8 Click for Details"));
 		menu.addMenuClickHandler(7, new MenuClickHandler() {
 			
 			@Override
@@ -71,9 +71,9 @@ public class BoosterMenu {
 	}
 
 	public static void showBoosterDetails(Player p, BoosterType type) {
-		ChestMenu menu = new ChestMenu("§3" + StringUtils.format(type.toString()) + " Boosters");
+		ChestMenu menu = new ChestMenu("§3Booster Overview");
 		
-		menu.addItem(1, new CustomItem(new MaterialData(Material.GOLD_INGOT), "§bBoosters (Money)", "§7Current Multiplier: §b" + Booster.getMultiplier(p.getUniqueId(), BoosterType.MONETARY), "", "§7\u21E8 Click for Details"));
+		menu.addItem(1, new CustomItem(new MaterialData(Material.GOLD_INGOT), "§bBoosters (Money)", "§7Current Multiplier: §b" + (QuickSell.cfg.getBoolean("boosters.use-percent-logic") ? QuickSell.toPercent(Booster.getMultiplier(p.getUniqueId(), BoosterType.MONETARY)) : DoubleHandler.getFancyDouble(Booster.getMultiplier(p.getUniqueId(), BoosterType.MONETARY))), "", "§7\u21E8 Click for Details"));
 		menu.addMenuClickHandler(1, new MenuClickHandler() {
 			
 			@Override
@@ -84,7 +84,7 @@ public class BoosterMenu {
 		});
 		
 		if (QuickSell.getInstance().isMCMMOInstalled()) {
-			menu.addItem(3, new CustomItem(new MaterialData(Material.IRON_SWORD), "§bBoosters (mcMMO)", "§7Current Multiplier: §b" + Booster.getMultiplier(p.getUniqueId(), BoosterType.MCMMO), "", "§7\u21E8 Click for Details"));
+			menu.addItem(3, new CustomItem(new MaterialData(Material.IRON_SWORD), "§bBoosters (mcMMO)", "§7Current Multiplier: §b" + (QuickSell.cfg.getBoolean("boosters.use-percent-logic") ? QuickSell.toPercent(Booster.getMultiplier(p.getUniqueId(), BoosterType.MCMMO)) : DoubleHandler.getFancyDouble(Booster.getMultiplier(p.getUniqueId(), BoosterType.MCMMO))), "", "§7\u21E8 Click for Details"));
 			menu.addMenuClickHandler(3, new MenuClickHandler() {
 				
 				@Override
@@ -96,7 +96,7 @@ public class BoosterMenu {
 		}
 		
 		if (QuickSell.getInstance().isPrisonGemsInstalled()) {
-			menu.addItem(5, new CustomItem(new MaterialData(Material.EMERALD), "§bBoosters (Gems)", "§7Current Multiplier: §b" + Booster.getMultiplier(p.getUniqueId(), BoosterType.PRISONGEMS), "", "§7\u21E8 Click for Details"));
+			menu.addItem(5, new CustomItem(new MaterialData(Material.EMERALD), "§bBoosters (Gems)", "§7Current Multiplier: §b" + (QuickSell.cfg.getBoolean("boosters.use-percent-logic") ? QuickSell.toPercent(Booster.getMultiplier(p.getUniqueId(), BoosterType.PRISONGEMS)) : DoubleHandler.getFancyDouble(Booster.getMultiplier(p.getUniqueId(), BoosterType.PRISONGEMS))), "", "§7\u21E8 Click for Details"));
 			menu.addMenuClickHandler(5, new MenuClickHandler() {
 				
 				@Override
@@ -107,7 +107,7 @@ public class BoosterMenu {
 			});
 		}
 		
-		menu.addItem(7, new CustomItem(new MaterialData(Material.EXP_BOTTLE), "§bBoosters (Experience)", "§7Current Multiplier: §b" + Booster.getMultiplier(p.getUniqueId(), BoosterType.EXP), "", "§7\u21E8 Click for Details"));
+		menu.addItem(7, new CustomItem(new MaterialData(Material.EXP_BOTTLE), "§bBoosters (Experience)", "§7Current Multiplier: §b" + (QuickSell.cfg.getBoolean("boosters.use-percent-logic") ? QuickSell.toPercent(Booster.getMultiplier(p.getUniqueId(), BoosterType.EXP)) : DoubleHandler.getFancyDouble(Booster.getMultiplier(p.getUniqueId(), BoosterType.EXP))), "", "§7\u21E8 Click for Details"));
 		menu.addMenuClickHandler(7, new MenuClickHandler() {
 			
 			@Override
@@ -138,7 +138,10 @@ public class BoosterMenu {
 	public static ItemStack getBoosterItem(Booster booster) {
 		List<String> lore = new ArrayList<String>();
 		lore.add("");
-		lore.add("§7Multiplier: §e" + booster.getMultiplier() + "x");
+		if (QuickSell.cfg.getBoolean("boosters.use-percent-logic")) 
+			lore.add("§7Boost: §e" + QuickSell.toPercent(booster.getMultiplier()));
+		else 
+			lore.add("§7Multiplier: §e" + booster.getMultiplier() + "x");
 		lore.add("§7Time Left: §e" + (booster.isInfinite() ? "Infinite": booster.formatTime() + "m"));
 		lore.add("§7Global: " + (booster.isPrivate() ? "§4§l\u2718": "§2§l\u2714"));
 		lore.add("");
@@ -146,12 +149,15 @@ public class BoosterMenu {
 		for (Map.Entry<UUID, Integer> entry: booster.getContributors().entrySet()) {
 			lore.add(" §8\u21E8 " + Bukkit.getOfflinePlayer(entry.getKey()).getName() + ": §a+" + entry.getValue() + "m");
 		}
-		return new CustomItem(new MaterialData(Material.EXP_BOTTLE), "§3" + booster.getMultiplier() + "x §b" + booster.getUniqueName(), lore.toArray(new String[lore.size()]));
+		return new CustomItem(new MaterialData(Material.EXP_BOTTLE), "§3" + (QuickSell.cfg.getBoolean("boosters.use-percent-logic") ? QuickSell.toPercent(booster.getMultiplier()) + " §b" : booster.getMultiplier() + "x §b") + booster.getUniqueName(), lore.toArray(new String[lore.size()]));
 	}
 
 	public static String getTellRawMessage(Booster booster) {
 		StringBuilder builder = new StringBuilder("§3" + booster.getMultiplier() + "x §b" + booster.getUniqueName() + "\n \n");
-		builder.append("§7Multiplier: §e" + booster.getMultiplier() + "x\n");
+		if (QuickSell.cfg.getBoolean("boosters.use-percent-logic")) 
+			builder.append("§7Boost: §e" + booster.getMultiplier());
+		else 
+			builder.append("§7Multiplier: §e" + booster.getMultiplier() + "x\n");
 		builder.append("§7Time Left: §e" + (booster.isInfinite() ? "Infinite": booster.formatTime() + "m") + "\n");
 		builder.append("§7Global: " + (booster.isPrivate() ? "§4§l\u2718": "§2§l\u2714") + "\n\n§7Contributors:\n");
 		for (Map.Entry<UUID, Integer> entry: booster.getContributors().entrySet()) {
